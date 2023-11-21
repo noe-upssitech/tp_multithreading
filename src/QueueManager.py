5f1fe74093c4d42ae8614bb6dcd4da8bef864323
Noe Marzat <noemarzat@gmail.com>
2023-11-21 14:39:33 +0100

    QueueManager Done

---
+++ src/QueueManager.py
@@ -0,0 +1,17 @@
+from multiprocessing import Queue
+from multiprocessing.context import BaseContext
+from multiprocessing.managers import BaseManager
+
+
+class QueueManager(BaseManager):
+    def __init__(
+        self,
+        address,
+        authkey: bytes | None = None,
+        serializer: str = "pickle",
+        ctx: BaseContext | None = None,
+    ) -> None:
+        super().__init__(address, authkey, serializer, ctx)
+
+        self.task_queue = Queue()
+        self.result_queue = Queue()
